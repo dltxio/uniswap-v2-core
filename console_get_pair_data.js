bfe22395a90bfc6f7bef81a7bdc3117abd8a93ab
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-12-06 09:43:12 +1000

    Update pair script

--- console_get_pair_data.js
+++ console_get_pair_data.js
@@ -5,13 +5,14 @@ const fs = require('fs')
 
 const Eth = require('ethjs')
 const Abi = require('ethjs-abi')
+const { TokenClass } = require('typescript')
 
 // const Sign = require("ethjs-signer").sign;
 // const HdKey = require("ethereumjs-wallet/hdkey");
 
 const tokenA = '0xbe0384bcCDCA2172585D0760d386D4e9DdeAB5C2' //AUD
 const tokenB = '0x535A548A5b736791c3ff7177AC33948e43518EA9' //YEN
-const to = '0x87227F5771eF47845118ecdb276D75f911aAaBD7'
+
 
 const contract_address = '0xEA4EEA1fF38b08794564F97349C66531f02d333C'
 const meta_mask_linux_account_2 = '0xC29082511fEBc2185986d341ee8be3c9B2c66b66' //Firefox
@@ -31,9 +32,18 @@ web3.eth.getTransactionCount(meta_mask_linux_account_2, 'pending').then(nonce =>
 
 
 const allPairsLength = async () => {
+  console.log('Pairs created')
   const result = await contract.methods.allPairsLength().call()
   console.log(result)
 }
 
 
+const getPair = async () => {
+  const result = await contract.methods.getPair(tokenA, tokenB).call()
+  console.log('Pairs')
+  console.log(result)
+}
+
+
 allPairsLength()
+getPair()
