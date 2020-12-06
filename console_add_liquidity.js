cca04cc7a99e375138d6af5802a1b674a068a9ad
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2020-12-07 09:46:37 +1000

    Change to handle factory hash etc

--- console_add_liquidity.js
+++ console_add_liquidity.js
@@ -14,7 +14,10 @@ const tokenB = '0x535A548A5b736791c3ff7177AC33948e43518EA9' //YEN
 const to = '0x87227F5771eF47845118ecdb276D75f911aAaBD7'
 
 // You can get the contract pair from "console_create_pair.js" and "console_get_pair_data.js"
-const contract_address = '0x1C583d76C45165C25c5aeea7a51415012122adB5'
+//const contract_address = '0x1C583d76C45165C25c5aeea7a51415012122adB5'
+//const contract_address = '0x2Ec22d2A7a7399f60A553a71BB0A22BA8B61f48D' //MONDAY 7/12 NO WETH
+const contract_address = '0x4C297C43fF3A4466e5a493db55952B4Af5572010' //MONDAY 7/12 REMOVE CREATE_2
+
 
 const meta_mask_mac_account_1 = '0x9943d42D7a59a0abaE451130CcfC77d758da9cA0'
 const meta_mask_mac_account_2 = '0x87227F5771eF47845118ecdb276D75f911aAaBD7'
@@ -25,7 +28,7 @@ const meta_mask_linux_account_2 = '0xC29082511fEBc2185986d341ee8be3c9B2c66b66' /
 const TWENTY_THIRTY = 1893456000
 
 let rawdata = fs.readFileSync('./build/HandleRouter.json')
-let HandleRouter = JSON.parse(rawdata)
+const HandleRouter = JSON.parse(rawdata)
 
 
 const web3 = new Web3(
@@ -35,8 +38,18 @@ const web3 = new Web3(
 const contract = new web3.eth.Contract(HandleRouter.abi, contract_address)
 const AUD_JPY_PAIR = '0x1C583d76C45165C25c5aeea7a51415012122adB5'
 
+const getFactory = async () => {
+  const result = await contract.methods.factory().call()
+  console.log(result)
+}
+
+const getWETH = async () => {
+  const result = await contract.methods.WETH().call()
+  console.log(result)
+}
+
 const _data = contract.methods
-  .addLiqudity(tokenA, tokenB, 5, 5, 1, 1, AUD_JPY_PAIR, TWENTY_THIRTY)
+  .addLiquidity(tokenA, tokenB, 5, 5, 1, 1, AUD_JPY_PAIR, TWENTY_THIRTY)
   .encodeABI()
 
 
@@ -55,7 +68,7 @@ web3.eth.getTransactionCount(meta_mask_linux_account_2, 'pending').then(nonce =>
   console.log(nonce)
 })
 
-// linux meta mask private key
+// // linux meta mask private key
 const PRIVATE_KEY = '0x2cccc34c1f3028d05b2a617d05ce60711258a0dd344a12f33dab6cc87aef6135'
 
 web3.eth.accounts
@@ -63,4 +76,8 @@ web3.eth.accounts
   .then(signed => {
     console.log(signed.rawTransaction)
     web3.eth.sendSignedTransaction(signed.rawTransaction).on('receipt', console.log)
-  })
+})
+
+getFactory()
+console.log('should be 0xEA4EEA1fF38b08794564F97349C66531f02d333C')
+// getWETH()
